dba8e7d2067441ba03a90d8622148bd06673e335
Philipp Anton <philippanton140396@gmail.com>
2022-05-03 18:43:10 +0200

    commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// get package into index.js
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const randomCol = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+function randomColor1() {
+  console.log(chalk.hex(randomCol).bold(row));
+}
+
+const row = `##############################\n##############################\n##############################\n#####                    #####\n#####      ${randomCol}       #####\n#####                    #####\n##############################\n##############################\n##############################`;
+
+if (process.argv < 3) {
+  console.log(chalk.hex(randomCol).bold(row));
+} else {
+  console.log(
+    chalk.hex(
+      randomColor({
+        luminosity: process.argv[3],
+        hue: process.argv[2],
+      }),
+    )(row),
+  );
+}
