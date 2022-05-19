87b4c5e37f2d972b6a52a128bf24a4e82fd52ad3
Philipp Anton <philippanton140396@gmail.com>
2022-05-19 11:58:26 +0200

    Simplified Code

--- index.js
+++ index.js
@@ -2,29 +2,11 @@
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-const randomCol = '#' + Math.floor(Math.random() * 16777215).toString(16);
+const randomCol = randomColor({
+  luminosity: process.argv[3],
+  hue: process.argv[2],
+});
 
 const row = `##############################\n##############################\n##############################\n#####                    #####\n#####      ${randomCol}       #####\n#####                    #####\n##############################\n##############################\n##############################`;
 
-const luminosity = randomColor({
-  luminosity: process.argv[3],
-  hue: process.argv[2],
-});
-
-const hue = randomColor({
-  luminosity: process.argv[3],
-  hue: process.argv[2],
-});
-
-if (!luminosity && !hue) {
-  console.log(chalk.hex(randomCol).bold(row));
-} else {
-  console.log(
-    chalk.hex(
-      randomColor({
-        luminosity: process.argv[3],
-        hue: process.argv[2],
-      }),
-    )(row),
-  );
-}
+console.log(chalk.hex(randomCol)(row));
